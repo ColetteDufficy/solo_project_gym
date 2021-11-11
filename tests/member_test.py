ffc4d292580241538ef507b96e6154b1f87927e1
colette dufficy <c.dufficy@gmail.com>
2021-11-11 23:29:30 +0000

    css on index page

--- tests/member_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from models.member import *
-
-class TestCalculator(unittest.TestCase):
-
-    def test_active_member_is_true(self):
-        result = active_membership(True)
-        self.assertEqual(True, result)
